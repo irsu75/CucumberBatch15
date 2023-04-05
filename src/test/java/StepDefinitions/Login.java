1fb2a868961078837f448fea8d18fa6334b635cc
irsu75 <irinasukhanova01@gmail.com>
2023-04-05 18:13:30 -0400

    CucumberBatch15

---
+++ src/test/java/StepDefinitions/Login.java
@@ -0,0 +1,44 @@
+package StepDefinitions;
+
+import Utils.CommonMethods;
+import Utils.ConfigReader;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+public class Login  extends CommonMethods {
+
+
+    @Given("open the browser and launch HRMS application")
+    public void open_the_browser_and_launch_hrms_application() {
+
+        openBrowserAndLaunchApplication();
+
+    }
+    @When("user enters valid email and valid password")
+    public void user_enters_valid_email_and_valid_password() {
+
+       driver.findElement(By.id("txtUsername")).sendKeys(ConfigReader.getPropertyValue("username"));
+       driver.findElement(By.id("txtPassword")).sendKeys(ConfigReader.getPropertyValue("password"));
+    }
+    @When("click on login button")
+    public void click_on_login_button() {
+
+      driver.findElement(By.id("btnLogin")).click();
+
+    }
+    @Then("user is logged in successfully into the application")
+    public void user_is_logged_in_successfully_into_the_application() {
+
+    boolean userloggedIn = driver.findElement(By.xpath("//a[contains(text(), 'Welcome')]")).isDisplayed();
+    if (userloggedIn){
+        System.out.println("User is logged in Successfully");
+    }
+
+}
+    @Then("Close the browser")
+    public void close_the_browser() {
+        closeBrowser();
+
+    }
+}
---
+++ src/test/java/TestRunner/SmokeRunner.java
@@ -0,0 +1,18 @@
+package TestRunner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+//@CucumberOptions(features = "src\\test\\resources\\Features" ,glue = "StepDefinitions",dryRun = false, tags="@testcase1 or @testcase2")
+@CucumberOptions(features = "src\\test\\resources\\Features",
+        glue = "StepDefinitions",
+        dryRun = false)
+
+public class SmokeRunner {
+}
+
+
+  // tags option will execute the tagged testcase as mentioned in your runner class
